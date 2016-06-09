902ba2f44401d5f5f1a848c29b3e10f05851aecf
IamTHEvilONE <jon.hemming@gmail.com>
2016-06-09 16:50:31 -0400

    Separate the reporting into a module of function calls
    make it cleaner so we can trigger things individually by breaking up the
    code

--- vrops-bundle-report.sh
+++ vrops-bundle-report.sh
@@ -2,8 +2,9 @@
 # Locate current location of the pull from github
 DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
-# Import the extraction module
+# Import the modules required
 . "$DIR/vrops-support-extraction.sh"
+. "$DIR/vrops-report-generator.sh"
 
 ###########################
 ## Main Function
@@ -15,53 +16,21 @@ vropsRenameZips;
 # Unzip the zip files if required
 vropsUnzip;
 
+#Once extracted, set some variables
 bundleTimeEpochA=`ls | grep cluster | cut -d\_ -f2`;
 bundleTimeEpoch=`expr $bundleTimeEpochA / 1000`;
-# echo $bundleTimeEpoch;
 bundleTime=`date -d @$bundleTimeEpoch`;
 bundleDate=`date -d @$bundleTimeEpoch +%Y-%m-%d`;
-# echo $bundleTime;
-echo "$bundleDate / $bundleTime - $bundleTimeEpoch";
+# echo "$bundleDate / $bundleTime - $bundleTimeEpoch";
 
 # Generate the list of node names based on the folders in the directory
 nodes=`ls | grep -v cluster`;
 
-echo "--== Bundle Info ==--">>01-basicinfo.txt;
-# what time was the bundle collected
-echo "The Support Bundle was created on $bundleTime" >> 01-basicinfo.txt;
-echo "">>01-basicinfo.txt;
-
-# what are the deployed builds?
-echo "What build numbers are in each node?" >> 01-basicinfo.txt;
-egrep RELEASENUMBER */slice-info/conf/.buildInfo >> 01-basicinfo.txt;
-echo "">>01-basicinfo.txt;
-
-# Locate the pak files that have been installed and then put them out to screen
-echo "What pak files are installed in the cluster? (Full Bundle maybe needed)" >> 01-basicinfo.txt;
-find . | grep pak | grep webapp | grep json | cut -d\_ -f4-| cut -d\. -f1 | sort -u >> 01-basicinfo.txt;
-echo "">>01-basicinfo.txt;
-
-echo "Rundown of each Node, in no particular order">> 01-basicinfo.txt;
-for node in $nodes
-do
-isNodeRC=`sed -nre 's/^remotecollectorroleenabled\s*=\s*(.+)\s*/\1/p' $node/slice-info/conf/utilities/sliceConfiguration/data/roleState.properties`;
-
-# processor count/speed
-        echo "--== $node ==--" >>01-basicinfo.txt;
-	vropsMajorVersion=`egrep RELEASENUMBER $node/slice-info/conf/.buildInfo | cut -d\: -f2- | cut -d\. -f1-3`;
-	vropsBuildNumber=`egrep RELEASENUMBER $node/slice-info/conf/.buildInfo | cut -d\. -f4`;
-	echo "Version & Build: $vropsMajorVersion $vropsBuildNumber" >> 01-basicinfo.txt;
-	nodeUUID=`egrep "slicedinstanceid =" $node/slice-info/conf/utilities/sliceConfiguration/data/platformState.properties | cut -d\  -f3`
-        echo "Node UUID: $nodeUUID" >> 01-basicinfo.txt;
-	egrep "enabled = " $node/slice-info/conf/utilities/sliceConfiguration/data/roleState.properties >> 01-basicinfo.txt;
-	echo "Processor Model:" `egrep "model name" $node/sysenv/cpuInfo.txt | tail -n1 | cut -d\: -f2-` >> 01-basicinfo.txt;
-        echo "Processor Count:" `egrep "processor" $node/sysenv/cpuInfo.txt | wc -l` >> 01-basicinfo.txt;
-	egrep "MemTotal" $node/sysenv/memInfo.txt | sort -u>> 01-basicinfo.txt;
-	grep  data-db $node/sysenv/df.txt >>01-basicinfo.txt;
-        echo "">> 01-basicinfo.txt;
-	head $node/sysenv/top.txt |tail -n6>>01-basicinfo.txt;
-	
-	echo "">> 01-basicinfo.txt;
-done
-
+# Generate the report header
+reportHeader $bundleTime;
+# Get build info
+reportBuildsAndPaks;
+# generate node specs
+reportNodeSpecs $nodes;
+# view the report
 less 01-basicinfo.txt;
---
+++ vrops-report-generator.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+# define the report filename
+report="00-vROpsSupportBundleReport.txt";
+errorLog="errorReport.txt";
+
+############################################
+# Create the header, requires $bundleTime
+############################################
+function reportHeader() {
+local importedArgs=( $1 );
+local bundleTime="${importedArgs[0]}";
+shift;
+
+echo "#####################################################" >> $report;
+echo "# vRealize Operations Manager Support Bundle Report #">> $report;
+echo "#####################################################" >> $report;
+echo "">> $report;
+
+if [ ! -z "$bundle" ];then
+    echo "--== Bundle Info ==--">> $report;
+    # what time was the bundle collected
+    echo "The Support Bundle was created on $bundleTime" >> $report;
+    echo "">>$report;
+else
+	echo "Nothing passed to reportHeader" >> $errorLog;
+fi
+}
+
+############################################
+# Report on what Paks are installed
+############################################
+function reportBuildsAndPaks() {
+# local importedArgs=( $1 );
+# local bundleTime="${importedArgs[0]}";
+# shift;
+
+# what are the deployed builds?
+echo "What build numbers are deployed in the cluster? (Supported configs should only show 1 build below)";
+egrep RELEASENUMBER */slice-info/conf/.buildInfo | awk -F"RELEASENUMBER:" '{ print $2 }' | sort -u >> $report;
+echo "">>$report;
+
+# Locate the pak files that have been installed and then put them out to screen
+echo "What pak files are installed in the cluster? (Full Bundle suggested)" >> $report;
+find . | grep pak | grep webapp | grep json | cut -d\_ -f4-| cut -d\. -f1 | sort -u >> $report;
+echo "">>$report;
+}
+
+############################################
+# Create the header, requires $bundleTime
+############################################
+function reportNodeSpecs() {
+local importedArgs=( $1 );
+local nodes="${importedArgs[0]}";
+shift;
+
+echo "Rundown of each Node, in no particular order">> $report;
+for node in $nodes
+do
+isNodeRC=`sed -nre 's/^remotecollectorroleenabled\s*=\s*(.+)\s*/\1/p' $node/slice-info/conf/utilities/sliceConfiguration/data/roleState.properties`;
+
+# processor count/speed
+        echo "--== $node ==--" >>$report;
+	vropsMajorVersion=`egrep RELEASENUMBER $node/slice-info/conf/.buildInfo | cut -d\: -f2- | cut -d\. -f1-3`;
+	vropsBuildNumber=`egrep RELEASENUMBER $node/slice-info/conf/.buildInfo | cut -d\. -f4`;
+	echo "Version & Build: $vropsMajorVersion $vropsBuildNumber" >> $report;
+	nodeUUID=`egrep "slicedinstanceid =" $node/slice-info/conf/utilities/sliceConfiguration/data/platformState.properties | cut -d\  -f3`
+        echo "Node UUID: $nodeUUID" >> $report;
+	egrep "enabled = " $node/slice-info/conf/utilities/sliceConfiguration/data/roleState.properties >> $report;
+	echo "Processor Model:" `egrep "model name" $node/sysenv/cpuInfo.txt | tail -n1 | cut -d\: -f2-` >> $report;
+        echo "Processor Count:" `egrep "processor" $node/sysenv/cpuInfo.txt | wc -l` >> $report;
+	egrep "MemTotal" $node/sysenv/memInfo.txt | sort -u>> $report;
+	grep  data-db $node/sysenv/df.txt >>$report;
+        echo "">> $report;
+	head $node/sysenv/top.txt |tail -n6>>$report;
+	
+	echo "">> $report;
+done
+}
